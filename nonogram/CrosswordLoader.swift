bdd6433afe3e2033db6ba1a75df9fe732587bef9
Denis Morozov <kramig@yandex-team.ru>
2023-01-22 20:31:03 +0600

    show list of crosswords

---
+++ nonogram/CrosswordLoader.swift
@@ -0,0 +1,133 @@
+//
+//  CrosswordLoader.swift
+//  nonogram
+//
+//  Created by Denis S. Morozov on 22.01.2023.
+//
+
+import Foundation
+
+class CrosswordLoader {
+    func load(url: URL, completion: @escaping (_ horizontalDefs: [[Field.Definition]],
+                                               _ verticalDefs: [[Field.Definition]],
+                                               _ solution: [[Int]],
+                                               _ colors: [Field.Color]) -> Void, failure: @escaping () -> Void) {
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+
+                var d: [[Int]] = []
+
+                if var s = String(data: data, encoding: .utf8) {
+                    s = s.matching("d=\\[\\[.+?\\]\\]").first!
+                    s = s.replacingOccurrences(of: "d=[[", with: "")
+                    s = s.replacingOccurrences(of: "]]", with: "")
+                    let rows = s.split(separator: "],[")
+
+                    for row in rows {
+                        var arr: [Int] = []
+                        for ch in row.split(separator: ",") {
+                            arr.append(Int(ch)!)
+                        }
+                        d.append(arr)
+                    }
+                }
+
+                var E: [[Int]] = []
+
+                let D: Int = d[1][0] % d[1][3] + d[1][1] % d[1][3] - d[1][2] % d[1][3]
+                let C: Int = d[2][0] % d[2][3] + d[2][1] % d[2][3] - d[2][2] % d[2][3]
+                let Aa: Int = d[3][0] % d[3][3] + d[3][1] % d[3][3] - d[3][2] % d[3][3]
+                var colors: [String] = []
+
+                for x in 5..<(Aa + 5) {
+                    let Ea = d[x][0] - d[4][1]
+                    let Fa = d[x][1] - d[4][0]
+                    let Ga = d[x][2] - d[4][3]
+
+                    var s1 = String(format:"%02X", Ea + 256)
+                    s1 = String(s1[(s1.index(s1.startIndex, offsetBy: 1))..<s1.endIndex])
+
+                    var s2 = String(format:"%02X", ((Fa + 256) << 8) + Ga)
+                    s2 = String(s2[(s2.index(s1.startIndex, offsetBy: 1))..<s2.endIndex])
+
+                    colors.append(s1 + s2)
+                }
+
+                for w in 0..<C {
+                    E.append([])
+                    for _ in 0..<D {
+                        E[w].append(0)
+                    }
+
+                }
+                let V = Aa + 5
+                let Ha = d[V][0] % d[V][3] * (d[V][0] % d[V][3]) + d[V][1] % d[V][3] * 2 + d[V][2] % d[V][3]
+                let Ia: [Int] = d[V + 1]
+
+                for x in (V + 2)..<(V + 1 + Ha) {
+                    let start: Int = (d[x][0] - Ia[0] - 1)
+                    let end: Int = (d[x][0] - Ia[0] + d[x][1] - Ia[1] - 1)
+                    for v in start..<end {
+                        E[d[x][3] - Ia[3] - 1][v] = d[x][2] - Ia[2]
+                    }
+                }
+
+                var verticals: [[Field.Definition]] = []
+                var horizontal: [[Field.Definition]] = []
+
+                for row in E {
+                    var n = 0
+                    var color: String = ""
+                    var horizontalRow: [Field.Definition] = []
+                    var prevCh: Int = -1
+                    for ch in row {
+                        if prevCh != ch && prevCh != -1 && prevCh != 0 {
+                            horizontalRow.append(.init(color: .init(rgb: color), n: n))
+                            n = 0
+                        }
+                        if ch != 0 {
+                            n += 1
+                            color = colors[ch - 1]
+                        }
+                        prevCh = ch
+                    }
+                    if n != 0 {
+                        horizontalRow.append(.init(color: .init(rgb: color), n: n))
+                    }
+                    horizontal.append(horizontalRow)
+                }
+
+                for i in 0..<E[0].count {
+                    var n = 0
+                    var color: String = ""
+                    var verticalRow: [Field.Definition] = []
+                    var prevCh: Int = -1
+                    for j in 0..<E.count {
+                        let ch = E[j][i]
+                        if prevCh != ch && prevCh != -1 && prevCh != 0 {
+                            verticalRow.append(.init(color: .init(rgb: color), n: n))
+                            n = 0
+                        }
+                        if ch != 0 {
+                            n += 1
+                            color = colors[ch - 1]
+                        }
+                        prevCh = ch
+                    }
+                    if n != 0 {
+                        verticalRow.append(.init(color: .init(rgb: color), n: n))
+                    }
+                    verticals.append(verticalRow)
+                }
+
+                completion(horizontal, verticals, E, colors.map({
+                    Field.Color(rgb: $0)
+                }))
+            }
+            catch {
+                failure()
+            }
+        }
+    }
+}
--- nonogram/MenuViewController.swift
+++ nonogram/MenuViewController.swift
@@ -7,10 +7,89 @@
 
 import Foundation
 import UIKit
+import SwiftSoup
 
 protocol MenuViewControllerDelegate: AnyObject {
-    func menuViewController(_: MenuViewController, load: URL)
-    func menuViewControllerContinue(_: MenuViewController)
+    func menuViewController(_: MenuViewController, selectWithUrl: URL)
+}
+
+struct Item {
+    var url: URL
+    var iocnURL: URL
+    var name: String
+    var image: UIImage?
+}
+
+class ListLoader {
+    private let url = "https://www.nonograms.ru/nonograms2/p/"
+    private var isLoading = false
+
+    func loadPage(_ page: Int, completaion: @escaping (_ items: [Item]?) -> Void) {
+        if isLoading {
+            return
+        }
+        isLoading = true
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: URL(string: self.url + "\(page)")!)
+
+                if let s = String(data: data, encoding: .utf8) {
+                    let doc: Document = try SwiftSoup.parse(s)
+                    let nonogramList = try doc.select("table.nonogram_list").first()!
+
+                    var result: [Item] = []
+
+                    for tr in try nonogramList.select("tr") {
+                        let nonogramImg = try tr.select("td.nonogram_img").first()
+                        let nonogramDescr = try tr.select("td.nonogram_descr").first()
+                        if let nonogramImg = nonogramImg, let _ = nonogramDescr {
+                            let urlString = try nonogramImg.select("a").first()!.attr("href")
+                            var iconUrlString = try nonogramImg.select("img").first()!.attr("src")
+                            iconUrlString = iconUrlString.replacingOccurrences(of: "_0.png", with: "_1.png")
+                            let name = try nonogramImg.select("img").first()!.attr("title")
+                            result.append(Item(
+                                url: URL(string: urlString)!,
+                                iocnURL: URL(string: iconUrlString)!,
+                                name: name
+                            ))
+                        }
+                    }
+
+                    let group = DispatchGroup()
+
+                    result.enumerated().forEach { index, item in
+                        group.enter()
+                        DispatchQueue.global().async {
+                            if let data = try? Data(contentsOf: item.iocnURL) {
+                                if let image = UIImage(data: data) {
+                                    result[index].image = image
+                                }
+                            }
+                            group.leave()
+                        }
+                    }
+
+                    group.wait()
+
+                    DispatchQueue.main.async {
+                        self.isLoading = false
+                        completaion(result)
+                    }
+
+                } else {
+                    DispatchQueue.main.async {
+                        self.isLoading = false
+                        completaion(nil)
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.isLoading = false
+                    completaion(nil)
+                }
+            }
+        }
+    }
 }
 
 class MenuViewController: UIViewController {
@@ -21,47 +100,116 @@ class MenuViewController: UIViewController {
     let loadButton = UIButton()
     let continueButton = UIButton()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let listLoader = ListLoader()
 
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        loadButton.translatesAutoresizingMaskIntoConstraints = false
-        continueButton.translatesAutoresizingMaskIntoConstraints = false
+    private var currentPage = 1
+    private var items: [Item] = []
 
-        textField.backgroundColor = .lightGray
-
-        view.addSubview(textField)
-        view.addSubview(loadButton)
-        view.addSubview(continueButton)
-
-        loadButton.addTarget(self, action: #selector(tapLoad), for: .touchUpInside)
-        continueButton.addTarget(self, action: #selector(tapContinue), for: .touchUpInside)
-
-        loadButton.setTitle("Загрузить", for: .normal)
-        loadButton.setTitleColor(.black, for: .normal)
-        continueButton.setTitle("Продолжить", for: .normal)
-        continueButton.setTitleColor(.black, for: .normal)
-
-        NSLayoutConstraint.activate([
-            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            textField.widthAnchor.constraint(equalToConstant: 400),
-
-            loadButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 50),
-            loadButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            continueButton.topAnchor.constraint(equalTo: loadButton.bottomAnchor, constant: 50),
-            continueButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        ])
+    @objc private func appMovedToForeground() {
+        loadCurrentPage()
     }
 
-    @objc private func tapLoad() {
-        if let url = URL(string: textField.text ?? "") {
-            delegate?.menuViewController(self, load: url)
+    private func show(items: [Item]) {
+        self.items = items
+
+        stackView.arrangedSubviews.forEach {
+            $0.removeFromSuperview()
+        }
+
+        items.enumerated().forEach { index, item in
+            let itemView = ItemView()
+            itemView.imageView.image = item.image
+            itemView.nameLabel.text = item.name
+            itemView.tag = index
+
+            stackView.addArrangedSubview(itemView)
+
+            let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapItem(_:)))
+            itemView.addGestureRecognizer(tapGR)
         }
     }
 
-    @objc private func tapContinue() {
-        delegate?.menuViewControllerContinue(self)
+    let stackView = UIStackView()
+
+    private func loadCurrentPage() {
+        listLoader.loadPage(currentPage) { items in
+            if let items = items {
+                self.show(items: items)
+            } else {
+                self.loadCurrentPage()
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            scrollView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+        ])
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector:#selector(appMovedToForeground),
+                                       name: UIApplication.willEnterForegroundNotification, object: nil)
+
+        loadCurrentPage()
+    }
+
+    @objc private func tapItem(_ tapGR: UITapGestureRecognizer) {
+        let index = tapGR.view?.tag ?? 0
+        let url = items[index].url
+        delegate?.menuViewController(self, selectWithUrl: url)
+    }
+}
+
+class ItemView: UIView {
+    let imageView = UIImageView()
+    let nameLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        nameLabel.numberOfLines = 0
+        nameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        nameLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+
+        addSubview(imageView)
+        addSubview(nameLabel)
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
